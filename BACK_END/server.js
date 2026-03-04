56746b2f9a98f16db4bb7662bd627dda05d01fa8
Marfidha <marfidhaap@gmail.com>
2026-03-04 23:43:01 +0530

    prepare backend for render deploy

--- BACK_END/server.js
+++ BACK_END/server.js
@@ -1,10 +1,11 @@
-import express from "express"
 import dotenv from "dotenv"
+dotenv.config()
+import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 import usermodel from "./models/Users.js"
 // import UpdateUser from "../FRONT_END/src/components/UpdateUser.js"
-dotenv.config()
+
 
 const port=process.env.PORT
 
@@ -13,10 +14,11 @@ const port=process.env.PORT
 
 const app=express()
 app.use(cors({
-  origin: ['http://localhost:5173/','http://localhost:5173','http://localhost:5174','http://localhost:5174/'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+   origin: "https://marfidha.github.io"
+  // origin: ['http://localhost:5173/','http://localhost:5173','http://localhost:5174','http://localhost:5174/'],
+  // methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  // allowedHeaders: ['Content-Type', 'Authorization'],
+  // credentials: true
 }));
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URL).then(()=>{
